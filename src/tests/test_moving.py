48c1e1ad5033009f1444576d3e02e5f5b1cc53d7
Miikka Seppi <miikka.seppi123@gmail.com>
2023-04-25 06:19:48 +0300

    added tests

--- src/tests/test_moving.py
+++ src/tests/test_moving.py
@@ -2,7 +2,7 @@ import unittest
 from game_logic import *
 
 
-class TestTetris(unittest.TestCase):
+class TestPieces(unittest.TestCase):
     def setUp(self):
         print("Set up succesful")
 
@@ -11,3 +11,21 @@ class TestTetris(unittest.TestCase):
         test_y = piece.y
         piece.down()
         assert piece.y > test_y
+
+    def test_pieces_move_left(self):
+        piece = Pieces()
+        test_x = piece.x
+        piece.left()
+        assert piece.x < test_x
+
+    def test_pieces_move_right(self):
+        piece = Pieces()
+        test_x = piece.x
+        piece.right()
+        assert piece.x > test_x
+
+    def test_pieces_rotate(self):
+        piece = Pieces()
+        test_shape = piece.shape
+        piece.rotate()
+        assert piece.shape != test_shape
