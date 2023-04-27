5958f1340a8f6b9f993747a12696466688c455cd
Miikka Seppi <miikka.seppi123@gmail.com>
2023-04-27 20:47:30 +0300

    fixed variables in tests

--- src/tests/test_moving.py
+++ src/tests/test_moving.py
@@ -8,21 +8,21 @@ class TestPieces(unittest.TestCase):
 
     def test_pieces_move_down(self):
         piece = Pieces()
-        test_y = piece.y
+        test_y = piece.y_value
         piece.down()
-        assert piece.y > test_y
+        assert piece.y_value > test_y
 
     def test_pieces_move_left(self):
         piece = Pieces()
-        test_x = piece.x
+        test_x = piece.x_value
         piece.left()
-        assert piece.x < test_x
+        assert piece.x_value < test_x
 
     def test_pieces_move_right(self):
         piece = Pieces()
-        test_x = piece.x
+        test_x = piece.x_value
         piece.right()
-        assert piece.x > test_x
+        assert piece.x_value > test_x
 
     def test_pieces_rotate(self):
         piece = Pieces()
@@ -32,5 +32,7 @@ class TestPieces(unittest.TestCase):
 
     def test_pieces_collision(self):
         piece = Pieces()
-        piece.y = WINDOW_HEIGHT - BSIZE * 5
+        piece.y = WINDOW_HEIGHT - BSIZE
+        for i in range(20):
+            piece.down()
         assert piece.collision() == True
