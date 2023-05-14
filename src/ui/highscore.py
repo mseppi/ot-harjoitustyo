8ded8c9029e4e748d4093e7cbff94bb84f43efaf
Miikka Seppi <miikka.seppi123@gmail.com>
2023-05-14 12:54:52 +0300

    filled text class

--- src/ui/highscore.py
+++ src/ui/highscore.py
@@ -1,6 +1,7 @@
 import pygame
 from variables.config import *
 from variables.constants import *
+from variables.text import Text
 import os
 
 def highscore(screen):
@@ -9,29 +10,12 @@ def highscore(screen):
     Args:
         screen (pygame.Surface): The screen to display the highscore on
     """
-    dir = "/home/mseppi/ot-harjoitustyo/src"
-    highscore_file = os.path.join(dir,"variables", "highscore.txt")
     process = True
-    highscore = []
-    with open(highscore_file, "r")as f:
-        for line in f:
-            name, score = line.strip().split(",")
-            highscore.append((name, int(score)))
-
     
     while process:
         screen.fill((BLACK))
-        font = pygame.font.Font('freesansbold.ttf', 32)
-        text = font.render("Highscore", True, (255, 255, 255))
-        textRect = text.get_rect()
-        textRect.center = (UI_WINDOW_WIDTH // 2, UI_WINDOW_HEIGHT // 2 - 200)
-        screen.blit(text, textRect)
-        for i in range(len(highscore)):
-            text = font.render(f"{i+1}. {highscore[i][0]}: {highscore[i][1]}", True, (255, 255, 255))
-            textRect = text.get_rect()
-            textRect.center = (UI_WINDOW_WIDTH // 2, UI_WINDOW_HEIGHT // 2 - 150 + 50*i)
-            screen.blit(text, textRect)
-        pygame.display.update()
+        highscore = Text(screen)
+        highscore.highscore()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 process = False
--- src/variables/text.py
+++ src/variables/text.py
@@ -1,6 +1,7 @@
 import pygame
 from variables.config import *
 from variables.constants import *
+import os
 
 
 class Text:
@@ -13,6 +14,14 @@ class Text:
             screen (pygame.Surface): The screen to draw the text on
         """
         self.screen = screen
+        dir = "/home/mseppi/ot-harjoitustyo/src"
+        self.highscore_file = os.path.join(dir,"variables", "highscore.txt")
+        self.highscore_list = []
+        with open(self.highscore_file, "r")as f:
+            for line in f:
+                name, score = line.strip().split(",")
+                self.highscore_list.append((name, int(score)))
+
 
     def game_over(self):
         font = pygame.font.Font('freesansbold.ttf', 100)
@@ -20,4 +29,18 @@ class Text:
         textRect = text.get_rect()
         textRect.center = (UI_WINDOW_WIDTH // 2, UI_WINDOW_HEIGHT // 2 - 100)
         self.screen.blit(text, textRect)
+        pygame.display.update()
+
+    def highscore(self):
+        self.screen.fill((BLACK))
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        text = font.render("Highscore", True, (255, 255, 255))
+        textRect = text.get_rect()
+        textRect.center = (UI_WINDOW_WIDTH // 2, UI_WINDOW_HEIGHT // 2 - 200)
+        self.screen.blit(text, textRect)
+        for i in range(len(self.highscore_list)):
+            text = font.render(f"{i+1}. {self.highscore_list[i][0]}: {self.highscore_list[i][1]}", True, (255, 255, 255))
+            textRect = text.get_rect()
+            textRect.center = (UI_WINDOW_WIDTH // 2, UI_WINDOW_HEIGHT // 2 - 150 + 50*i)
+            self.screen.blit(text, textRect)
         pygame.display.update()
